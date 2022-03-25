c1b0cb2cce9808e6b1c1e157bcc8c88bf8b12548
Timofey Chuchkanov <crt0r.9@yahoo.com>
2022-03-25 22:45:39 +0300

    feat(src/App.svelte-src/data/couch.js): fetch data from the database

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,34 @@
 <script>
-  import { Router } from "@roxi/routify";
-  import { routes } from "../.routify/routes";
+    import { fetchGroups, fetchNames, fetchTimetables, fetchTeachersTimetables, fetchOrder } from "./data/couch";
+    import { onMount } from 'svelte';
+    import { Router } from "@roxi/routify";
+    import { routes } from "../.routify/routes";
+
+    let groups;
+    let names;
+    let timetables;
+    let teachersTimetables;
+    let order;
+
+    onMount(async function() {
+            groups = await fetchGroups(); 
+            names = await fetchNames(); 
+            timetables = await fetchTimetables(); 
+            teachersTimetables = await fetchTeachersTimetables();  
+            order = await fetchOrder();
+    });
 </script>
 
-<Router {routes} />
+{#if !groups && !names}
+        Loading...
+{:else}
+        { JSON.stringify(groups) }
+        <hr>
+        { JSON.stringify(names) }
+        <hr>
+        { JSON.stringify(timetables) }
+        <hr>
+        { JSON.stringify(teachersTimetables ) }
+        <hr>
+        { JSON.stringify(order) }
+{/if}
---
+++ src/data/couch.js
@@ -0,0 +1,86 @@
+/**
+    Copyright (C) 2022 Timofey Chuchkanov
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https:www.gnu.org/licenses/>.
+ */
+
+import { user, paths, baseUrl } from '../config.json';
+
+const buildFullUrl = (path) => `${ baseUrl + path }/`;
+const groupsFullPath = buildFullUrl(paths.groups);
+const namesFullPath = buildFullUrl(paths.names);
+const timetablesFullPath = buildFullUrl(paths.timetables);
+const teachersTimetablesFullPath = buildFullUrl(paths.teachersTimetables);
+const orderFullPath = buildFullUrl(paths.order);
+
+async function fetchTeachersTimetables() {
+    const response = await fetchFromDb(teachersTimetablesFullPath);
+
+    return removeInternalFields(response);
+}
+
+async function fetchTimetables() {
+    const response = await fetchFromDb(timetablesFullPath);
+
+    return removeInternalFields(response);
+}
+
+async function fetchOrder() {
+    const resonse = await fetchFromDb(orderFullPath);
+
+    return removeInternalFields(resonse);
+}
+
+async function fetchNames() {
+    const result = await fetchFromDb(namesFullPath);
+    const data = result.data;
+    return data;
+}
+
+async function fetchGroups() {
+    const result = await fetchFromDb(groupsFullPath);
+    const data = result.data;
+    return data;
+}
+
+async function fetchFromDb(fullPath) {
+    const credentials = btoa(`${ user.name }:${ user.password }`);
+    const headers = new Headers();
+
+    headers.append('Accept', 'application/json');
+    headers.append('Authorization', `Basic ${ credentials }`);
+
+    const response = await fetch(fullPath, {
+            method: 'GET',
+            credentials: 'include',
+            headers
+    });
+
+    const json = await response.json();
+    const cleanJson = removeInternalFields(json);
+
+    return cleanJson;
+}
+
+// removeInternalFields: -> Object Object
+// Remove _rev and _id fields
+function removeInternalFields(obj) {
+    const { _rev, _id, ...cleanData } = obj;
+
+    return cleanData;
+}
+
+export {
+    fetchGroups, fetchNames, fetchOrder, fetchTeachersTimetables, fetchTimetables
+};
