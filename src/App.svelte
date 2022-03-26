ad7f1923a986d2706e723381dff3e56837aa1894
Timofey Chuchkanov <crt0r.9@yahoo.com>
2022-03-26 17:48:48 +0300

    feat(components/LoadingIndicator.svelte-App.svelte): add loading indicator component

--- src/App.svelte
+++ src/App.svelte
@@ -1,34 +1,21 @@
 <script>
-    import { fetchGroups, fetchNames, fetchTimetables, fetchTeachersTimetables, fetchOrder } from "./data/couch";
+    import { fetchGroups, fetchNames, fetchTimetables } from './data/couch';
+    import LoadingIndicator from './components/LoadingIndicator.svelte';
     import { onMount } from 'svelte';
-    import { Router } from "@roxi/routify";
-    import { routes } from "../.routify/routes";
+    import { Router } from '@roxi/routify';
+    import { routes } from '../.routify/routes';
 
     let groups;
     let names;
-    let timetables;
-    let teachersTimetables;
-    let order;
 
     onMount(async function() {
             groups = await fetchGroups(); 
             names = await fetchNames(); 
-            timetables = await fetchTimetables(); 
-            teachersTimetables = await fetchTeachersTimetables();  
-            order = await fetchOrder();
     });
 </script>
 
 {#if !groups && !names}
-        Loading...
+    <LoadingIndicator></LoadingIndicator>
 {:else}
-        { JSON.stringify(groups) }
-        <hr>
-        { JSON.stringify(names) }
-        <hr>
-        { JSON.stringify(timetables) }
-        <hr>
-        { JSON.stringify(teachersTimetables ) }
-        <hr>
-        { JSON.stringify(order) }
+    
 {/if}
