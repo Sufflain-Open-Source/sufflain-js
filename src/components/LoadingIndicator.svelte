ebb41f6c650aa611d67f6fddf4cbc57fde7f5c7d
Timofey Chuchkanov <crt0r.9@yahoo.com>
2022-04-10 22:11:04 +0300

    feat: add loading indicator style

--- src/components/LoadingIndicator.svelte
+++ src/components/LoadingIndicator.svelte
@@ -1,3 +1,37 @@
-<div>
-    Loading...
+<div class="loadingContainer">
+    <div class="loadingIndicator"></div>
 </div>
+
+<style>
+    div.loadingContainer {
+        display: grid;
+        place-items: center;
+        height: 100%;
+        width: 100%;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+    }
+
+    div.loadingIndicator {
+        height: 40px;
+        width: 40px;
+        background-repeat: no-repeat;
+        background-size: contain;
+        background-position: center;
+        background-image: url(/img/loading.svg);
+        animation: spin 0.5s ease-in-out infinite;
+    }
+
+    @keyframes spin {
+        from {
+            transform: rotate(0deg);
+        }
+
+        to {
+            transform: rotate(360deg);
+        }
+    }
+
+</style>
