39118b5811400d56f0eb2d982ccc0c6ca4f9875d
Timofey Chuchkanov <crt0r.9@yahoo.com>
2022-07-12 20:42:59 +0300

    make injection script bourne shell compliant
    Also added package.json package name and updated the dependencies

--- inject-analytics-config.sh
+++ inject-analytics-config.sh
@@ -1,14 +1,15 @@
-#!/bin/bash
+#!/bin/sh
 
-CONFIG=$1
+CONFIG="$1"
 CONFIG_PLACEHOLDER=">>config<<"
 
-function alter_target_file() {
-    sed -i "s@$1@$2@" dist/index.html
+alter_target_file() 
+{
+    sed -i '' "s@$1@$2@" dist/index.html
 }
 
-if [ $CONFIG == "restore" ]; then
+if [ "$CONFIG" = "restore" ]; then
     alter_target_file "'config', '.*'" "'config', '$CONFIG_PLACEHOLDER'"
 else
-    alter_target_file $CONFIG_PLACEHOLDER $(cat $CONFIG)
+    alter_target_file $CONFIG_PLACEHOLDER `cat $CONFIG`
 fi
